97067b172ae2861c5c4f908f309125c561f4a39e
Vatsal-Malaviya <vmalaviy@asu.edu>
2024-08-12 09:18:10 -0700

    fix: sys path to connect src files

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,5 +1,7 @@
 import os
 import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pytest
 from torch.utils.data import DataLoader
 from omegaconf import OmegaConf
