1325ae7b7a7b73b7879451af832c39ba1e1699c5
Vatsal-Malaviya <vmalaviy@asu.edu>
2024-08-13 21:27:41 -0700

    feat: project head and clip module added

--- src/models.py
+++ src/models.py
@@ -29,3 +29,40 @@ class TextTransformer(nn.Module):
     def forward(self, input_ids, attention_mask):
         output = self.model(input_ids, attention_mask)
         return output.last_hidden_state[:, 0, :]
+
+
+class ProjectionHead(nn.Module):
+    """
+    Projection head for contrastive learning
+    """
+
+    def __init__(self, embedding_dim, projection_dim) -> None:
+        super().__init__()
+        self.fc = nn.Linear(embedding_dim, projection_dim)
+
+    def forward(self, x):
+        return self.fc(x)
+
+
+class CLIP(nn.Module):
+    """
+    CLIP model
+    """
+
+    def __init__(self, image_encoder, text_encoder, projection_dim) -> None:
+        super().__init__()
+        self.image_encoder = image_encoder
+        self.text_encoder = text_encoder
+        self.image_projection = ProjectionHead(
+            image_encoder.model.embed_dim, projection_dim
+        )
+        self.text_projection = ProjectionHead(
+            text_encoder.model.config.hidden_size, projection_dim
+        )
+
+    def forward(self, image, input_ids, attention_mask):
+        image_features = self.image_encoder(image)
+        text_features = self.text_encoder(input_ids, attention_mask)
+        image_projection = self.image_projection(image_features)
+        text_projection = self.text_projection(text_features)
+        return image_projection, text_projection
