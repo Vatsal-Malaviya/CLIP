561bb8c22362aae217a4fa60fc62c9cf1dcb6dff
Vatsal-Malaviya <vmalaviy@asu.edu>
2024-08-17 19:23:57 -0700

    fix: added data check for nan
    additionally update coresponding to hydra

--- scripts/flickr_cleaning.py
+++ scripts/flickr_cleaning.py
@@ -6,14 +6,14 @@ from omegaconf import DictConfig
 @hydra.main(config_path="../configs", config_name="config", version_base=None)
 def clean_data(cfg: DictConfig) -> None:
     # Load the data
-    data_file = hydra.utils.to_absolute_path(cfg.dataset.data_file)
+    data_file = hydra.utils.to_absolute_path(cfg.dataset.annotation_file)
     flickr = pd.read_csv(data_file, sep="|")
 
     # Clean the data
     flickr.columns = [col.strip() for col in flickr.columns]
     for col in flickr.columns:
         flickr[col] = flickr[col].str.strip()
-
+    flickr.dropna(inplace=True)
     # Save the cleaned data
     flickr.to_csv(data_file, sep="|", index=False)
     print(f"Data cleaned and saved to {data_file}")
